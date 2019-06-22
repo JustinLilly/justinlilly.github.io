4b0967fc6cee3a9b12fcefb3a5447fc53a24001d
JustinLilly <39202473+JustinLilly@users.noreply.github.com>
2019-06-22 00:36:48 -0600

    Fixed up a ton of CSS

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -4,16 +4,20 @@
 console.log('My javascript is being read.');    
 
 // Variables for Function Use
-const temp = 31;
-const speed = 5;
-const direction = "SSW"; //Set your own value
+let temp = 31;
+let speed = 5;
+let direction = "SSW"; //Set your own value
+let weatherDesc = "Rainy";
+let weatherCondition = "";
+
 
 buildWC(speed, temp);
 windDial(direction);
+getCondition(weatherDesc);
 
 // Calculate the Windchill
 function buildWC(speed, temp) {
-    const feelTemp = document.getElementById("feelTemp");
+    let feelTemp = document.getElementById("feelTemp");
     
     // Compute the windchill
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
@@ -33,7 +37,7 @@ function buildWC(speed, temp) {
 // Wind Dial Function
 function windDial(direction){
     // Get the wind dial container
-    const dial = document.getElementById("dial");
+    let dial = document.getElementById("dial");
 
     // Determine the dial class
     switch (direction){
@@ -74,4 +78,137 @@ function windDial(direction){
         dial.setAttribute("class", "w");
         break;
    }
+}
+
+
+// Get Condition function
+function getCondition(weatherDesc) {
+    // Determine weather condition
+    if(weatherDesc == "whatever") {
+        weatherCondition = "Clear";
+    }
+    if(weatherDesc == "whatever") {
+        weatherCondition = "Cloud";
+    }
+    if(weatherDesc == "whatever") {
+        weatherCondition = "Fog";
+    }
+    if(weatherDesc == "whatever") {
+        weatherCondition = "Rain";
+    }
+    if(weatherDesc == "whatever") {
+        weatherCondition = "Snow";
+    }
+    return weatherCondition;
+}
+function changeSummaryImage(weatherCondition) {
+    // Change summary image
+    switch (weatherCondition) {
+        case "clear":
+        case "Clear":
+        weatherView.setAttribute("class", "clear"); break;
+        case "fog":
+        case "Fog":
+        case "foggy":
+        case "Foggy":
+        weatherView.setAttribute("class", "fog"); break;
+        case "rain":
+        case "Rain":
+        case "rainy":
+        case "Rainy":
+        weatherView.setAttribute("class", "rain"); break;
+        case "cloudy":
+        case "Cloudy":
+        case "cloud":
+        case "Cloud":
+        weatherView.setAttribute("class", "cloudy"); break;
+        case "snow":
+        case "Snow":
+        case "snowy":
+        case "Snowy":
+        weatherView.setAttribute("class", "snow"); break;
+    }
+}
+
+
+
+
+/*********************  Not Ready for this yet ***************/
+
+let weatherURL = "path/to/weather.json";
+function fetchData(weatherURL){
+  let cityName = 'Greenville'; // The data we want from the weather.json file
+  fetch(weatherURL)
+  .then(function(response) {
+  if(response.ok){
+  return response.json();
+  }
+  throw new ERROR('Network response was not OK.');
+  })
+  .then(function(data){
+    // Check the data object that was retrieved
+    console.log(data);
+    // data is the full JavaScript object, but we only want the greenville part
+    // shorten the variable and focus only on the data we want to reduce typing
+    let g = data[cityName];
+
+    // ************ Get the content ******************************
+
+    // Get the location data
+    let locName = g.City;
+    let locState = g.State;
+    // Put them together
+    let fullName = locName+', '+locState;
+    // See if it worked
+    console.log('fullName is: '+fullName);
+
+    // Get the temperature data
+    let locTemp = g.Temp;
+
+    // Get the wind data 
+    let wind = g.Wind;
+
+    // Get the current conditions
+
+
+    // Get the hourly data 
+
+    // ************ Display the content ******************************
+    // Set the title with the location name at the first
+    // Gets the title element so it can be worked with
+    let pageTitle = document.getElementById('page-title');
+    // Create a text node containing the full name 
+    let fullNameNode = document.createTextNode(fullName);
+    // inserts the fullName value before any other content that might exist
+    pageTitle.insertBefore(fullNameNode, pageTitle.childNodes[0]);
+    // When this is done the title should look something like this:
+    // Greenville, SC | The Weather Site
+
+    // Set the Location information
+    // Get the h1 to display the city location
+    let contentHeading = document.getElementById('contentHeading');
+    contentHeading.innerHTML = fullName;
+    // The h1 in main h1 should now say "Greenville, SC"
+
+
+    // Set the temperature information
+    document.getElementById("curTemp").innerHTML = locTemp;
+
+    // Set the wind information
+    document.getElementById("wind-speed").innerHTML = wind;
+
+    // Set the current conditions information
+
+
+    // Set the hourly temperature information
+
+
+    // Change the status of the containers
+    contentContainer.setAttribute('class', ''); // removes the hide class
+    statusContainer.setAttribute('class', 'hide'); // hides the status container
+  })
+  .catch(function(error){
+  console.log('There was a fetch problem: ', error.message);
+  statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+  })
 }
